2aef749637c6977782be3c6802f58793adeda96f
krio10 <krio10@yandex.ru>
2020-05-05 02:07:42 +0300

    remove csslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-const cleanCSS = require('gulp-clean-css');
-const csslint = require('gulp-csslint');
 const concat = require('gulp-concat');
 const minifyCSS = require('gulp-minify-css');
 
@@ -20,16 +18,8 @@ gulp.task('browser-sync', function() {
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('css', () => {
-    gulp.src('src/css/*.css')
-      .pipe(csslint())
-      .pipe(csslint.formatter());
-  });
-
 gulp.task('minify', () => {
     return gulp.src('src/css/*.css')
-      .pipe(csslint())
-      .pipe(csslint.formatter())
       .pipe(concat('styles.min.css'))
       .pipe(minifyCSS({
         keepBreaks: true
