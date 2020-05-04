7a97a1af4ed7031cc4d536717d6d3a360aecee29
krio10 <krio10@yandex.ru>
2020-05-04 22:19:42 +0300

    Create right structure

--- gulpfile.js
+++ gulpfile.js
@@ -21,24 +21,24 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('minify-css', () => {
-  return gulp.src('css/*.css')
+  return gulp.src('src/css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('css', () => {
-    gulp.src('css/*.css')
+    gulp.src('src/css/*.css')
       .pipe(csslint())
       .pipe(csslint.formatter());
   });
 
 gulp.task('minify', () => {
-    return gulp.src('css/*.css')
+    return gulp.src('src/css/*.css')
       .pipe(csslint())
       .pipe(csslint.formatter())
       .pipe(concat('styles.min.css'))
       .pipe(minifyCSS({
         keepBreaks: true
         }))
-      .pipe(gulp.dest('css'));
+      .pipe(gulp.dest('dist/css'));
   });
