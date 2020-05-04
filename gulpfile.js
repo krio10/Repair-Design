51ca5a19d9234a46f0acce55f2ea233c73c99403
krio10 <krio10@yandex.ru>
2020-05-04 17:53:32 +0300

    include css directory & style.css.min

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+var cleanCSS = require('gulp-clean-css');
+var csslint = require('gulp-csslint');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
 
 gulp.task('hello', function(done) {
     console.log('Привет, мир');
@@ -15,3 +19,26 @@ gulp.task('browser-sync', function() {
     });
     gulp.watch("app/*.html").on('change', browserSync.reload);
 });
+
+gulp.task('minify-css', () => {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('css', () => {
+    gulp.src('css/*.css')
+      .pipe(csslint())
+      .pipe(csslint.formatter());
+  });
+
+gulp.task('minify', () => {
+    return gulp.src('css/*.css')
+      .pipe(csslint())
+      .pipe(csslint.formatter())
+      .pipe(concat('styles.min.css'))
+      .pipe(minifyCSS({
+        keepBreaks: true
+        }))
+      .pipe(gulp.dest('css'));
+  });
