29e83fb4c552f8d186a4d0669ebc2f2dad042160
krio10 <krio10@yandex.ru>
2020-05-17 01:41:31 +0300

    feat: add js fuctions for close modal dialog with Escape key and mouse outside dialog

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const closeBtn = document.querySelector('.modal__close');
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
-  }
+  };
+
   modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
   });
   closeBtn.addEventListener('click', switchModal);
+
+  // функция закрытия модального окна по клику мышью за его пределами
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      switchModal();
+    }
+  }
+
+  // функция закрытия модального окна по нажатию на клавишу Escape
+  document.onkeydown = function(event) {
+    event = event || window.event;
+    var isEscape = false;
+    if ("key" in event) {
+        isEscape = (event.key === "Escape" || event.key === "Esc");
+    } else {
+        isEscape = (event.keyCode === 27);
+    }
+    if (isEscape) {
+      switchModal();
+    }
+  };
 });
