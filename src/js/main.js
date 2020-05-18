6878af84faae401412f6b0cc083e6b2d0d044138
krio10 <krio10@yandex.ru>
2020-05-19 00:33:50 +0300

    fix: fixed float scrollTol function

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,8 @@
-/*document.addEventListener("DOMContentLoaded", function(event) { 
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  };
-
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
-  closeBtn.addEventListener('click', switchModal);
-
-  // функция закрытия модального окна по клику мышью за его пределами
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      switchModal();
-    }
-  }
-
-  // функция закрытия модального окна по нажатию на клавишу Escape
-  document.onkeydown = function(event) {
-    event = event || window.event;
-    var isEscape = false;
-    if ("key" in event) {
-        isEscape = (event.key === "Escape" || event.key === "Esc");
-    } else {
-        isEscape = (event.keyCode === 27);
-    }
-    if (isEscape) {
-      switchModal();
-    }
-  }
-});
-*/
-
 $(document).ready(function () {
   var modal = $('.modal'),
       modalBtn = $('[data-toggle=modal]'),
       closeBtn = $('.modal__close'),
       backTop = $('.back-top')
-      scrollUp = $('back-top__scroll-up');
   
   modal.hide();
   backTop.hide();
@@ -83,12 +46,11 @@ $(document).ready(function () {
     }
   });
 
-
-  // Функция плавногй прокрутки страницы вверх при нажатии кнопки со стрелкой вверх
-  scrollUp.click(function (){
+  // Функция плавной прокрутки страницы вверх при нажатии кнопки со стрелкой вверх
+  backTop.click(function (){
     $("body,html").animate({
       scrollTop:0
-    }, 5000);
+    }, 2000);
     return false;
   });  
 });
