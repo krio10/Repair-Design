8e9be28e7bb998fa211e0ec71bae2923087bdc98
krio10 <krio10@yandex.ru>
2020-05-31 21:25:17 +0300

    feat: add map with yandex map API

--- src/js/main.js
+++ src/js/main.js
@@ -318,4 +318,36 @@ $(document).ready(function () {
   // маска для номера телефона
   $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
   
+  // создание карты
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [47.259879, 39.720168],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Офис №345',
+            balloonContent: 'Вход со двора'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/pin.svg',
+            // Размеры метки.
+            iconImageSize: [32, 32],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+    myMap.geoObjects
+        .add(myPlacemark);
+  });
 });
